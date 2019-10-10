8df93be6a57817f9f821793747382cf869048044
Dawid Stoga <dawid.stoga@gmail.com>
2019-10-10 23:42:54 +0200

    Swagger: Response types. CustomConvention for response types

--- SwaggerDemo/Controllers/BookController.cs
+++ SwaggerDemo/Controllers/BookController.cs
@@ -1,14 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
+﻿using System.Collections.Generic;
+using CoreWebApp.Api;
 using Microsoft.AspNetCore.Mvc;
 
 namespace SwaggerDemo.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [ApiConventionType(typeof(CustomApiConvention))]
     public class BookController : ControllerBase
     {
         // GET api/book
@@ -19,6 +17,7 @@ namespace SwaggerDemo.Controllers
         }
 
         // GET api/book/5
+        [ApiConventionMethod(typeof(CustomApiConvention), "MySpecialConvention")]
         [HttpGet("{id}")]
         public ActionResult<string> Get(int id)
         {
@@ -26,6 +25,9 @@ namespace SwaggerDemo.Controllers
         }
 
         // POST api/book
+        [ProducesResponseType(404)]
+        [ProducesResponseType(201)]
+        [ProducesResponseType(500)]
         [HttpPost]
         public void Post([FromBody] string value)
         {
@@ -33,13 +35,13 @@ namespace SwaggerDemo.Controllers
 
         // PUT api/book/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public void BookPut(int id, [FromBody] string value)
         {
         }
 
         // DELETE api/book/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public void BookDelete(int id)
         {
         }
     }
---
+++ SwaggerDemo/CustomApiConvention.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ApiExplorer;
+
+namespace CoreWebApp.Api
+{
+    public static class CustomApiConvention
+    {
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status208AlreadyReported)]
+        [ProducesResponseType(StatusCodes.Status508LoopDetected)]
+        [ApiConventionNameMatch(ApiConventionNameMatchBehavior.Prefix)]
+        public static void Book([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]int id)
+        {
+
+        }
+
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public static void MySpecialConvention([ApiConventionNameMatch(ApiConventionNameMatchBehavior.Suffix)]int id)
+        {
+
+        }
+
+    }
+}
