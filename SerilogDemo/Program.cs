a783a088b32eda7405235692963f785070880a3d
Dawid Stoga <dawid.stoga@gmail.com>
2019-10-11 09:52:43 +0200

    Serilog: Configure logger to use console and File

--- SerilogDemo/Program.cs
+++ SerilogDemo/Program.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using Serilog;
 
 namespace SerilogDemo
 {
@@ -14,7 +15,17 @@ namespace SerilogDemo
     {
         public static void Main(string[] args)
         {
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.Console()
+                .WriteTo.File("logs\\log.txt", rollingInterval: RollingInterval.Hour)
+                .CreateLogger();
+
+            Log.Information("Start Application");
+            Log.Debug("Debug message");
+
             CreateWebHostBuilder(args).Build().Run();
+
+            Log.CloseAndFlush();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
