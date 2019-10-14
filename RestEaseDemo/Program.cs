8b3e43c3aaafe25d47b647e42b218a21a1cbe71e
stogad <dstoga@pgs-soft.com>
2019-10-14 10:47:54 +0200

    RestEase: Initial proj restored, RestEase install from nuget

--- RestEaseDemo/Program.cs
+++ RestEaseDemo/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace RestEase
+namespace RestEaseDemo
 {
     public class Program
     {
