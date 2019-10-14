95795c099141c6bbcdaeabcc1b4e622c0a581e32
stogad <dstoga@pgs-soft.com>
2019-10-14 11:14:45 +0200

    RestEase: Prepared Api and service Without RestEase so far !!!

---
+++ RestEaseDemo/Services/CommentsService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using RestEaseDemo.Model;
+
+namespace RestEaseDemo.Services
+{
+    public interface ICommentsService
+    {
+        Task<IEnumerable<Comment>> GetComments();
+        Task<Comment> GetCommentItemAsync(int id);
+    }
+
+    public class CommentsService : ICommentsService
+    {
+        private readonly HttpClient _httpClient;
+        private readonly string _remoteServiceBaseUrl = "https://jsonplaceholder.typicode.com/Comments";
+
+        public CommentsService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+        public async Task<Comment> GetCommentItemAsync(int id)
+        {
+            var ressponseString = await _httpClient.GetStringAsync($"{_remoteServiceBaseUrl}/{id}");
+            var Comments = JsonConvert.DeserializeObject<Comment>(ressponseString);
+
+            return Comments;
+        }
+
+        public async Task<IEnumerable<Comment>> GetComments()
+        {
+            var ressponseString = await _httpClient.GetStringAsync(_remoteServiceBaseUrl);
+            var Comments = JsonConvert.DeserializeObject<IEnumerable<Comment>>(ressponseString);
+
+            return Comments;
+        }
+    }
+}
--- RestEaseDemo/Startup.cs
+++ RestEaseDemo/Startup.cs
@@ -10,6 +10,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using RestEaseDemo.Model;
+using RestEaseDemo.Services;
 
 namespace RestEaseDemo
 {
@@ -25,6 +27,7 @@ namespace RestEaseDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddHttpClient<ICommentsService, CommentsService>();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
