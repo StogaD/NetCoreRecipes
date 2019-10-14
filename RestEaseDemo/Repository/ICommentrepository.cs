fee1c86382c0518ce2260f62ce0bc8101414bbe0
stogad <dstoga@pgs-soft.com>
2019-10-14 11:36:22 +0200

    RestEase: Added different possibilities to added api for resst ease

--- RestEaseDemo/Repository/ICommentrepository.cs
+++ RestEaseDemo/Repository/ICommentrepository.cs
@@ -8,6 +8,15 @@ using RestEaseDemo.Model;
 namespace RestEaseDemo.Repository
 {
 
+
+    public class SearchParams
+    {
+        public string Term { get; set; }
+        public string Mode { get; set; }
+    }
+
+    [Header("User-Agent", "Demo-RestEase")]
+    [Header("Content-Type", "application/json")]
     public interface ICommentRepository
     {
         [Get("/comments")]
@@ -15,5 +24,36 @@ namespace RestEaseDemo.Repository
 
         [Get("/comments/{id}")]
         Task<Comment> GetComment([Path] int id);
+
+
+
+        [Get("/comments/{id}")]
+        Task<Comment> GetCommentV2([Path(Format = "X2")] int id);
+
+        //Serialization of Variable Query Parameters
+        [Get("/comments/search")]
+        Task<Comment> SearchComment([Query(QuerySerializationMethod.Serialized)] SearchParams param);
+        // in service: await api.SearchAsync(new SearchParams() { Term = "foo", Mode = "basic" });
+
+        //Query Parameters Map
+        [Get("/comments/searchBlog")]
+        Task<Comment> SearchBlogPostsAsync([QueryMap] IDictionary<string, string[]> filters);
+        /*in service: 
+            var filters = new Dictionary<string, string[]>()
+            {
+                { "title", new[] { "bobby" } },
+                { "tag", new[] { "c#", "programming" } }
+            };
+            Requests http://api.example.com/search?title=bobby&tag=c%23&tag=programming
+            var searchResults = await api.SearchBlogPostsAsync(filters);
+            */
+
+
+        //Raw Query String Parameters
+        [Get("searchbyQueryString")]
+        Task<Comment> SearchAsync([RawQueryString] string customFilter);
+
+        //var searchResults = await api.SearchAsync("id=2");
+
     }
 }
