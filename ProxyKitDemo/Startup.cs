25dbaeee50982d38a088a9700b79f858f9c8ea60
stogad <dstoga@pgs-soft.com>
2019-10-16 10:02:13 +0200

    ProxyKit: Simple proxy forward - host replacement

--- ProxyKitDemo/Startup.cs
+++ ProxyKitDemo/Startup.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ProxyKit;
 
 namespace ProxyKitDemo
 {
@@ -25,6 +28,7 @@ namespace ProxyKitDemo
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddProxy();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -41,8 +45,17 @@ namespace ProxyKitDemo
                 app.UseHsts();
             }
 
+            app.RunProxy(ProxyHandler);
             app.UseHttpsRedirection();
             app.UseMvc();
+        }
+
+        private Task<HttpResponseMessage> ProxyHandler(HttpContext httpContext)
+        {
+            string host = Configuration["ProxyKit:Host"];
+            var forwardContext = httpContext.ForwardTo(host);
+            var response = forwardContext.Send();
+            return response;
         }
     }
 }
