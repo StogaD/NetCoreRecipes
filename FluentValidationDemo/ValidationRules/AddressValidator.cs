a74cd331d856cad4422405270a9441844264da19
stogad <dstoga@pgs-soft.com>
2019-10-17 10:44:05 +0200

    FValidation: Conditional rule (2) + Cascade

--- FluentValidationDemo/ValidationRules/AddressValidator.cs
+++ FluentValidationDemo/ValidationRules/AddressValidator.cs
@@ -14,10 +14,17 @@ namespace FluentValidationDemo.ValidationRules
 
             RuleFor(address => address.Postcode).NotNull();
             RuleFor(address => address.City)
+                .Cascade(CascadeMode.StopOnFirstFailure)
                 .NotEmpty()
                 .WithMessage("City is required")
                 .Must(y => y.Equals("Wrocław", StringComparison.InvariantCultureIgnoreCase))
                 .When(x => x.Postcode != null && x.Postcode.StartsWith("50-"));
+
+            //Other conditional rule
+            When(addr => addr.Postcode != null && addr.Postcode.StartsWith("50-"), () =>
+            {
+                RuleFor(address => address.City).NotNull().Must(x => x.Equals("Wroclaw", StringComparison.InvariantCultureIgnoreCase));
+            });
 
             RuleFor(address => address.StreetLines).NotNull().ForEach(x => x.MaximumLength(20));
             RuleFor(address => address.Number).GreaterThan(0);
