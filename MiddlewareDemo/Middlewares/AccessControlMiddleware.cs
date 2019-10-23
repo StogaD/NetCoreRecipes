87be5f2b988640d0c7cc326a047c4c6a7d7e18f9
stogad <dstoga@pgs-soft.com>
2019-10-23 13:53:35 +0200

    #Middleware:  add simple (transparent) middleware

--- MiddlewareDemo/Middlewares/AccessControlMiddleware.cs
+++ MiddlewareDemo/Middlewares/AccessControlMiddleware.cs
@@ -2,10 +2,22 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace MiddlewareDemo.Middlewares
 {
     public class AccessControlMiddleware
     {
+        private readonly RequestDelegate _next;
+
+        public AccessControlMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public Task Invoke(HttpContext httpContext)
+        {
+            return _next(httpContext);
+        }
     }
 }
