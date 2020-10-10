cf1409d86a589b4cfa8962032d7bda5aa005dc1b
DAWID <DAWID@DESKTOP-JCJMJB1>
2020-10-10 22:22:59 +0200

    quickStart1

---
+++ IdentityServer4Demo/QuickStart1/IdServerHost/Config.cs
@@ -0,0 +1,43 @@
+﻿using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+
+namespace IdServerHost
+{
+  internal class Config
+  {
+    internal static IEnumerable<IdentityResource> GetIdentityResources()
+    {
+      return new List<IdentityResource>
+      {
+
+      };
+    }
+
+    internal static IEnumerable<ApiResource> GetApis()
+    {
+      return new List<ApiResource>
+     {
+        new ApiResource("api1", "My Api")
+     };
+    }
+
+    internal static IEnumerable<Client> GetClients()
+    {
+      return new List<Client>
+      {
+        new Client
+        {
+          ClientId = "client",
+          AllowedGrantTypes = GrantTypes.ClientCredentials,
+          ClientSecrets =
+          {
+            new Secret("secret".Sha256())
+          },
+          AllowedScopes = {"api1"}
+        }
+      };
+
+    }
+  }
+}
